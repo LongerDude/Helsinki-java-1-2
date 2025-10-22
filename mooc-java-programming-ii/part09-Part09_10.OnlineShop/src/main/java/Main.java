c5206e7a7e95b0a745f294c0e3563712fa4edc27
Salmane Khalili <salmajo2001@gmail.com>
2025-10-22 23:32:26 +0100

    redid some methods in shoppingcart with streams to practice

--- mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/Main.java
@@ -6,15 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         // Test your app here
-        Warehouse warehouse = new Warehouse();
-        warehouse.addProduct("coffee", 5, 10);
-        warehouse.addProduct("milk", 3, 20);
-        warehouse.addProduct("cream", 2, 55);
-        warehouse.addProduct("bread", 7, 8);
-
-        Scanner scanner = new Scanner(System.in);
-
-        Store store = new Store(warehouse, scanner);
-        store.shop("John");
+        ShoppingCart cart = new ShoppingCart();
+        cart.add("bread", 1);
+        cart.add("bread2", 15);
+        cart.add("bread3", 21);
+        cart.print();
+        System.out.println(cart.price());
     }
 }
--- mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/ShoppingCart.java
+++ mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/ShoppingCart.java
@@ -8,34 +8,44 @@ import java.util.Map;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author salma
  */
 public class ShoppingCart {
+
     private Map<String, Item> cart;
+
     public ShoppingCart() {
         this.cart = new HashMap<>();
-        }
-    public void add(String product, int price){
-        if (this.cart.containsKey(product)){
+    }
+
+    public void add(String product, int price) {
+        if (this.cart.containsKey(product)) {
             this.cart.get(product).increaseQuantity();
             return;
         }
         this.cart.put(product, new Item(product, 1, price));
     }
-    public void print(){
-        for (String product:this.cart.keySet()){
-            System.out.println(this.cart.get(product).toString());
-        }
+
+    public void print() {
+        //for (String product:this.cart.keySet()){
+        //    System.out.println(this.cart.get(product).toString());
+        //}
+        this.cart.values().stream().forEach(item -> System.out.println(item.toString()));
     }
-    public int price(){
-        int sum = 0;
-        for (Item product:this.cart.values()){
-            sum += product.price();
-        }
-        return sum;
+
+    public int price() {
+        //int sum = 0;
+        //for (Item product : this.cart.values()) {
+         //   sum += product.price();
+        //}
+        //return sum;
+
+        int total = this.cart.values().stream().mapToInt(Item::price).sum();
+        int total2 = this.cart.values().stream().map(Item::price).reduce(0, Integer::sum);
+        return total2;
+        
     }
     
 }

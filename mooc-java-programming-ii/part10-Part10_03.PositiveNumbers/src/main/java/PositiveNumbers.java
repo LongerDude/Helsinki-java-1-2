68ba234a946208d834f87be1d5dd8f294d710298
Salmane Khalili <salmajo2001@gmail.com>
2025-10-22 20:25:46 +0100

    implementing methods with streams, as well as looking at lamba expressions

--- mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -8,7 +9,20 @@ public class PositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(3);
+        list.add(-5);
+        List List = positive(list);
+        System.out.println(List);
 
+    }
+    public static List<Integer> positive(List<Integer> numbers){
+        return numbers.stream()
+                .filter(s -> s>0)
+                .collect(Collectors.toList());
     }
 
 }

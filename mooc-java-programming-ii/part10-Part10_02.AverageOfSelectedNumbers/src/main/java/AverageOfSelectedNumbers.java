ef46607177634dd5cde25e793ae40c85d79f1409
Salmane Khalili <salmajo2001@gmail.com>
2025-10-22 18:57:11 +0100

    stream exercise where i calculate the avg of positive and negative numbers

--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -6,7 +6,25 @@ public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         // toteuta ohjelmasi tänne
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while (true){
+            String input = scanner.nextLine();
+            if(input.equals("end")){
+                System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+                String choice = scanner.nextLine();
+                if (choice.equals("n")){
+                    double negAvg = list.stream().mapToInt(s -> Integer.valueOf(s)).filter(s -> s< 0).average().getAsDouble();
+                    System.out.println("Average of the negative numbers: " + negAvg);
+                } else {
+                    double posAvg = list.stream().mapToInt(s -> Integer.valueOf(s)).filter(s -> s > 0).average().getAsDouble();
+                    System.out.println("Average of the positive numbers: " + posAvg);
+                }
+                break;
+            }
+            list.add(input);
+        }
 
     }
 }

68ca4396cadd23edca83269d74c91db8eb345059
Salmane Khalili <salmajo2001@gmail.com>
2025-10-27 21:32:35 +0100

    using super compareto methods of objects to compare inherting objects

--- mooc-java-programming-ii/part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
+++ mooc-java-programming-ii/part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
@@ -1,11 +1,15 @@
 
 
-public class Student {
+public class Student implements Comparable<Student> {
 
     private String name;
 
     public Student(String name) {
         this.name = name;
+    }
+    @Override
+    public int compareTo(Student student){
+        return name.compareTo(student.getName());
     }
 
     public String getName() {

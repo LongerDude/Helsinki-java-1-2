bd48ad04a6915b1df2aa0ddea9fdb6f1169243b2
Salmane Khalili <salmajo2001@gmail.com>
2025-10-28 21:07:28 +0100

    First exercise in regex, hated this syntax

--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,28 @@
 
 
 public class Checker {
+    public boolean isDayOfWeek(String string){
+        if (string.matches("mon|tue|wed|thu|fri|sat|sun")){
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    public boolean allVowels(String string) {
+        if (string.matches("[a|e|i|o|u]*")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean timeOfDay(String string) {
+        if (string.charAt(0) == '1' || string.charAt(0) == '0'){
+           return string.matches("[0-1][0-9]{1}:[0-5][0-9]{1}:[0-5][0-9]{1}");
+        } else if (string.charAt(0)== '2') {
+            return string.matches("[0-2][0-3]{1}:[0-5][0-9]{1}:[0-5][0-9]{1}");
+        } 
+        return false;      
+    }
 
 }
--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -5,5 +5,10 @@ public class MainProgram {
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        Checker checker = new Checker();
+        String text = "queue";
+        System.out.println(checker.allVowels(text));
+        String time = "07:23:05";
+        System.out.println(checker.timeOfDay(time));
     }
 }

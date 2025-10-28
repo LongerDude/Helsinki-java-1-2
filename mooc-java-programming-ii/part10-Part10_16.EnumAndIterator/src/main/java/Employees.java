50b33cbe21d836048ddbcb1dd4963a6139bee016
Salmane Khalili <salmajo2001@gmail.com>
2025-10-28 22:03:43 +0100

    Utilized iterators instead of foreach loops, with their build in methods, for a more efficient and clearer iteration, this is paired with enums

---
+++ mooc-java-programming-ii/part10-Part10_16.EnumAndIterator/src/main/java/Employees.java
@@ -0,0 +1,56 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author salma
+ */
+public class Employees {
+
+    private ArrayList<Person> employees;
+
+    public Employees() {
+        this.employees = new ArrayList<>();
+    }
+
+    public void add(Person personToAdd) {
+        employees.add(personToAdd);
+    }
+
+    public void add(List<Person> peopleToAdd) {
+        this.employees.addAll(peopleToAdd);
+    }
+
+    public void print() {
+        Iterator<Person> iterator = employees.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public void print(Education education) {
+        Iterator<Person> iterator = employees.iterator();
+        while (iterator.hasNext()) {
+            Person pers = iterator.next();
+            if (pers.getEducation() == education) {
+                System.out.println(pers);
+            }
+        }
+    }
+
+    public void fire(Education education) {
+        Iterator<Person> iterator = employees.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getEducation() == education) {
+                iterator.remove();
+            }
+        }
+    }
+}

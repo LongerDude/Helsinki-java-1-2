af5529b431a93b75c593b18ddf4c6d8e6831ec04
Salmane Khalili <salmajo2001@gmail.com>
2025-10-29 21:25:27 +0100

    Validating parameter input by throwing illigealargument exceptions

--- mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
+++ mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
@@ -3,6 +3,9 @@ package validating;
 public class Calculator {
 
     public int factorial(int num) {
+        if (num <0){
+            throw new IllegalArgumentException("negative numbers are not to be inputted");
+        }
 
         int answer = 1;
         for (int i = 1; i <= num; i++) {
@@ -13,6 +16,9 @@ public class Calculator {
     }
 
     public int binomialCoefficent(int setSize, int subsetSize) {
+        if (setSize < 0 || subsetSize < 0 || subsetSize > setSize){
+            throw new IllegalArgumentException("should be non negative and setsize larger than subsetsize");
+        }
 
         int numerator = factorial(setSize);
         int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
--- mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -7,6 +7,12 @@ public class Person {
 
     public Person(String name, int age) {
 
+        if (name == null || name.isEmpty() || name.length() > 40 ) {
+            throw new IllegalArgumentException("Name shouldn't be above 40 chars, not null, not empty");
+        }
+        if (age > 120 || age < 0) {
+            throw new IllegalArgumentException("Age should be between 0 and 120");
+        }
         this.name = name;
         this.age = age;
     }

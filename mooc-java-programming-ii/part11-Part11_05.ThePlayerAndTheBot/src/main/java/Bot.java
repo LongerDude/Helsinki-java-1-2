3af3a6c3666b3341b6c7cc61c067fdc88594abe4
Salmane Khalili <salmajo2001@gmail.com>
2025-10-29 14:11:56 +0100

    More UML, inheritence and Interfaces

---
+++ mooc-java-programming-ii/part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author salma
+ */
+public class Bot extends Player {
+    public Bot(String name){
+        super(name);
+    }
+    public void addMove(String move){
+        
+    }
+}

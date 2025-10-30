dd855db136526b99aa88c97924f38abf3d4c833f
Salmane Khalili <salmajo2001@gmail.com>
2025-10-30 16:58:40 +0100

    Observing datastructures again

--- mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -6,7 +6,35 @@ public class Program {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // test your method here
+        int[] numbers = {3, -1, 8, 4};
 
+        System.out.println(sum(numbers, 0, 0, 0, 0));
+        System.out.println(sum(numbers, 0, 0, 0, 10));
+        System.out.println(sum(numbers, 0, 1, 0, 10));
+        System.out.println(sum(numbers, 0, 1, -10, 10));
+        System.out.println(sum(numbers, -1, 999, -10, 10));
+
+    }
+
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
+        // check for valid lower boundary
+        if (fromWhere < 0) { 
+            fromWhere = 0;
+        }
+        // check for valid upper boundary
+        if (toWhere >= array.length) { 
+            toWhere = array.length;
+        }
+        // accumulator pattern
+        int sum = 0;
+
+        for (int i = fromWhere; i < toWhere; i++) {
+            if (array[i] >= smallest && array[i] <= largest) {
+                sum += array[i];
+            }
+        }
+
+        return sum;
     }
 
 }

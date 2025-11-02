e9f10df929f0971f8cc9628490a5985ab311f54c
Salmane Khalili <salmajo2001@gmail.com>
2025-11-02 17:01:08 +0100

    First exercise in event handling within a GUI

--- mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,39 @@
 package notifier;
+import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+public class NotifierApplication extends Application{
 
-public class NotifierApplication {
-
+    @Override
+    public void start(Stage window){
+        Button button = new Button("Update");
+        Label label = new Label("");
+        TextField field = new TextField();
+        
+        button.setOnAction((event) -> {
+           label.setText(field.getText());
+        });
+        
+        VBox layout = new VBox();
+        
+        layout.getChildren().addAll(field,button,label);
+        
+        Scene scene = new Scene(layout);
+        
+        window.setScene(scene);
+        window.show();
+        
+        
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
+        
     }
 
 }

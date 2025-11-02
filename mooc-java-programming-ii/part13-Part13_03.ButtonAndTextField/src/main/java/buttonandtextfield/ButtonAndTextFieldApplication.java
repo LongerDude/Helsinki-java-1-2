63d6005127150f05cc4f24202480e6412aaaa32e
Salmane Khalili <salmajo2001@gmail.com>
2025-11-02 12:49:07 +0100

    More UI components

--- mooc-java-programming-ii/part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
+++ mooc-java-programming-ii/part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -1,11 +1,33 @@
 package buttonandtextfield;
 
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+public class ButtonAndTextFieldApplication extends Application{
 
-public class ButtonAndTextFieldApplication {
+    @Override
+    public void start(Stage window) {
+        Button button = new Button("Click me");
+        TextField field = new TextField();
+        
+        FlowPane layout = new FlowPane();
+        layout.getChildren().add(button);
+        layout.getChildren().add(field);
+        
+        Scene scene = new Scene(layout);
+        
+        window.setTitle("Hi there");
+        window.setScene(scene);
+        window.show();
+        
 
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndTextFieldApplication.class);
     }
 
 }

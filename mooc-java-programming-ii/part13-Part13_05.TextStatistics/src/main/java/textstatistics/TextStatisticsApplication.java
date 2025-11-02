13df681bcacd2036c1946be1ca80ed8285441820
Salmane Khalili <salmajo2001@gmail.com>
2025-11-02 14:45:25 +0100

    More UI components+layout

--- mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,36 @@
 package textstatistics;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
-public class TextStatisticsApplication {
+public class TextStatisticsApplication extends Application{
 
+    @Override
+    public void start(Stage window){
+        TextArea field = new TextArea();        
+        HBox trackers = new HBox();
+        BorderPane layout = new BorderPane();
+        trackers.getChildren().add(new Label("Letters: 0"));
+        trackers.getChildren().add(new Label("Words: 0"));
+        trackers.getChildren().add(new Label("The longest word is:"));
+        layout.setBottom(trackers);
+        layout.setCenter(field);
+        
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.show();
+        
+        
+        
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
     }
 
 }

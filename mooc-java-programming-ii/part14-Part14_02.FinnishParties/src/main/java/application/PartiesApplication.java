5d3b3cc80fbf0d1aab312a1f9b3b4eb335b47f15
salmane <salmajo2001@gmail.com>
2025-11-07 00:51:52 +0100

    Add layout

--- mooc-java-programming-ii/part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
+++ mooc-java-programming-ii/part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
@@ -15,13 +15,13 @@ public class PartiesApplication extends Application {
 
     @Override
     public void start(Stage stage) {
-        NumberAxis xAxis = new NumberAxis(1968,2008,4);
-        NumberAxis yAxis = new NumberAxis(0,40,10);
+        NumberAxis xAxis = new NumberAxis(1968, 2008, 4);
+        NumberAxis yAxis = new NumberAxis(0, 40, 10);
         xAxis.setLabel("Year");
         LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
         lineChart.setTitle("Relative support of the parties");
-        parseData("partiesdata.tsv",lineChart);
-        
+        parseData("partiesdata.tsv", lineChart);
+
         Scene view = new Scene(lineChart, 640, 480);
         stage.setScene(view);
         stage.show();
@@ -39,9 +39,7 @@ public class PartiesApplication extends Application {
                 String[] parts = row.split("\t"); // break it down
                 data.setName(parts[0]); // set name for the current graph
                 for (int i = 1; i < parts.length; i++) {  // add the data
-                    System.out.println(headerParts[i]);
-                    System.out.println(parts[i]);
-                    if (parts[i].equals("-")){
+                    if (parts[i].equals("-")) {
                         continue;
                     }
                     data.getData().add(new XYChart.Data(Integer.valueOf(headerParts[i]), Double.valueOf(parts[i])));
